b214bea377b2ef4a37aeeb7fb6ea4bd03f779274
Martin Shaw <mshaw@venditan.com>
2022-05-23 20:40:24 +0100

    Improve use of PSR0

--- lib/Vend/VenditanCommerceOutputInspector.php
+++ lib/Vend/VenditanCommerceOutputInspector.php
@@ -1,7 +1,7 @@
 <?php
 namespace Vend;
 
-class VenditanCommerceOutputInspector extends \Boris\DumpInspector {
+class VenditanCommerceOutputInspector extends \Boris\ExportInspector {
     public function objectVars($value) {
 //        if ($value instanceof User) {
 //            return array('user_id' => $value->getUserId());
